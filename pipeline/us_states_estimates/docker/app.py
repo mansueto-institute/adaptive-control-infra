dbf0b17a8eba5e44277de1e7040781801a2593af
stevenbuschbach <sbuschbach@uchicago.edu>
2020-08-18 09:01:29 -0500

    add pymc3

--- pipeline/us_states_estimates/docker/app.py
+++ pipeline/us_states_estimates/docker/app.py
@@ -1,13 +1,44 @@
 from flask import Flask, request
+import arviz as az
+import matplotlib.pyplot as plt
+import numpy as np
+
+import pymc3 as pm
+from pymc3 import  *
+
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
-def hello():
-    """Return a friendly HTTP greeting."""
-    who = request.args.get('who', 'World')
-    return f'Hello {who}!\n'
+def main():
+
+    print('Running on PyMC3 v{}'.format(pm.__version__))
+
+    size = 200
+    true_intercept = 1
+    true_slope = 2
+
+    x = np.linspace(0, 1, size)
+   
+    true_regression_line = true_intercept + true_slope * x           # y = a + b*x
+    y = true_regression_line + np.random.normal(scale=.5, size=size) # add noise
+
+    data = dict(x=x, y=y)
+
+    with Model() as model: 
+    
+        # Define priors
+        sigma = HalfCauchy('sigma', beta=10, testval=1.)
+        intercept = Normal('Intercept', 0, sigma=20)
+        x_coeff = Normal('x', 0, sigma=20)
+
+        # Define likelihood
+        likelihood = Normal('y', mu=intercept + x_coeff * x,
+                            sigma=sigma, observed=y)
+
+        # Inference!
+        trace = sample(3000) # draw 3000 posterior samples using NUTS sampling
 
 
 if __name__ == '__main__':
