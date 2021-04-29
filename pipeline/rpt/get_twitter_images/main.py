5989bc5e3c479ba1ba852d019c2e73691b486dc0
satej soman <satej.soman@gmail.com>
2021-04-29 18:44:47 -0500

    remove blocking calls, and add logging

--- pipeline/rpt/get_twitter_images/main.py
+++ pipeline/rpt/get_twitter_images/main.py
@@ -20,6 +20,7 @@ bucket = storage.Client().bucket(bucket_name)
 
 @app.route("/state/<state_code>")
 def generate_report(state_code: str):
+    print(f"Received request for {state_code}.")
     state = state_code_lookup[state_code]
     blobs = { 
         f"pipeline/est/{state_code}_state_Rt.csv"      : "/tmp/state_Rt.csv",
@@ -28,6 +29,7 @@ def generate_report(state_code: str):
     }
     for (blob_name, filename) in blobs.items():
         bucket.blob(blob_name).download_to_filename(filename)
+    print(f"Downloaded estimates for {state_code}.")
     
     state_Rt    = pd.read_csv("/tmp/state_Rt.csv",    parse_dates = ["dates"], index_col = 0)
     district_Rt = pd.read_csv("/tmp/district_Rt.csv", parse_dates = ["dates"], index_col = 0)
@@ -40,8 +42,8 @@ def generate_report(state_code: str):
         .adjust(left = 0.11, bottom = 0.16)
     plt.gcf().set_size_inches(3840/300, 1986/300)
     plt.savefig(f"/tmp/{state_code}_Rt_timeseries.png")
-    plt.show()
     plt.clf()
+    print(f"Generated timeseries plot for {state_code}.")
 
     gdf = gpd.read_file("/tmp/state.json")
     gdf["Rt"] = gdf.district.map(district_Rt)
@@ -54,6 +56,7 @@ def generate_report(state_code: str):
     plt.axis('off')
     plt.savefig(f"/tmp/{state_code}_Rt_choropleth.png", dpi = 300)
     plt.clf() 
+    print(f"Generated choropleth for {state_code}.")
 
     fig, ax = plt.subplots(1,1)
     ax.axis('tight')
@@ -64,13 +67,14 @@ def generate_report(state_code: str):
         if (row == 0):
             cell.set_text_props(fontfamily = plt.theme.label["family"], fontsize = plt.theme.label["size"])
     plt.PlotDevice().title(f"{state}: top districts by $R_t$", ha = "center", x = 0.5)
-    plt.show()
     plt.savefig(f"/tmp/{state_code}_Rt_top10.png", dpi = 600)
     plt.clf()
+    print(f"Generated top 10 district listing for {state_code}.")
 
     bucket.blob(f"pipeline/rpt/{state_code}_Rt_timeseries.png").upload_from_filename(f"/tmp/{state_code}_Rt_timeseries.png", content_type = "image/png")
     bucket.blob(f"pipeline/rpt/{state_code}_Rt_choropleth.png").upload_from_filename(f"/tmp/{state_code}_Rt_choropleth.png", content_type = "image/png")
     bucket.blob(f"pipeline/rpt/{state_code}_Rt_top10.png")     .upload_from_filename(f"/tmp/{state_code}_Rt_top10.png",      content_type = "image/png")
+    print(f"Uploaded artifacts for {state_code}.")
     return "OK!"
 
 if __name__ == "__main__":
