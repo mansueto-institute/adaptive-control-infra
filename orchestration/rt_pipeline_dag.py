cfee6d00b1acaa453f711958abef5918cc1036ff
satej soman <satej.soman@gmail.com>
2021-05-07 00:35:23 -0500

    assemble initial conditions

--- orchestration/rt_pipeline_dag.py
+++ orchestration/rt_pipeline_dag.py
@@ -4,8 +4,10 @@ import os
 
 import requests
 from airflow import models
+from airflow.models.connection import Connection
 from airflow.hooks.http_hook import HttpHook
 from airflow.operators.http_operator import SimpleHttpOperator
+from airflow.operators.dummy_operator import DummyOperator
 
 AUDIENCE_ROOT = os.environ["GCF_URL"]
 METADATA_ROOT = os.environ["METADATA"]
@@ -47,21 +49,30 @@ class CloudFunction(SimpleHttpOperator):
     # see 
     # 1) https://github.com/salrashid123/composer_gcf
     # 2) https://github.com/salrashid123/composer_gcf/blob/master/composer_to_gcf/to_gcf.py
+    # 3) https://cloud.google.com/run/docs/authenticating/service-to-service#python
+
+    def get_metadata_url(self):
+        return f"{METADATA_ROOT}{AUDIENCE_ROOT}/{self.endpoint}"
+    
     def execute(self, context):
-        token = requests.get(f"{METADATA_ROOT}{AUDIENCE_ROOT}/{self.endpoint}", headers = {"Metadata-Flavor": "Google"}).text
+        token = requests.get(self.get_metadata_url(), headers = {"Metadata-Flavor": "Google"}).text
         HttpHook(self.method, http_conn_id = self.http_conn_id)\
             .run(self.endpoint, self.data, {"Content-Type": "application/json", "Authorization": f"Bearer {token}"}, self.extra_options)
 
-class CloudRun(SimpleHttpOperator):
-    # see 
-    # 1) https://github.com/salrashid123/composer_gcf
-    # 2) https://github.com/salrashid123/composer_gcf/blob/master/composer_to_gcf/to_gcf.py
-    # 3) https://cloud.google.com/run/docs/authenticating/service-to-service#python
-    def execute(self, context):
-        cloud_run_url = "https://get-twitter-images-sipjq3uhla-uc.a.run.app"
-        token = requests.get(f"{METADATA_ROOT}{cloud_run_url}", headers = {"Metadata-Flavor": "Google"}).text
-        HttpHook(self.method, http_conn_id = self.http_conn_id)\
-            .run(self.endpoint, self.data, {"Content-Type": "application/json", "Authorization": f"Bearer {token}"}, self.extra_options)
+class CloudRun(CloudFunction):
+    def __init__(self, run_url, conn_id, *args, **kwargs):
+        conn = Connection(
+            conn_id   = conn_id,
+            conn_type = "http",
+            host      = run_url,
+            schema    = "https"
+        )
+        kwargs["http_conn_id"] = conn_id
+        super(CloudRun, self).__init__(*args, **kwargs)
+        self.run_url = run_url
+
+    def get_token(self):
+        return f"{METADATA_ROOT}{self.run_url}"
 
 def epi_step(state):
     return CloudFunction(
@@ -79,7 +90,8 @@ def create_Rt_report(state):
         method       = "GET",
         endpoint     = f"state/{state}",
         start_date   = datetime.datetime(2021, 4, 29),
-        http_conn_id = "cloud_run_create_report",
+        run_url      = "get-twitter-images-sipjq3uhla-uc.a.run.app",
+        conn_id      = "cloud_run_create_report",
         retries      = 3
     )
 
@@ -94,50 +106,61 @@ def tweet_Rt_report(state):
         retries      = 3
     )
 
-def forward_simulation(state):
-    return CloudFunction(
-        task_id      = f"tweet_report_{state}",
-        method       = "POST",
-        endpoint     = "STEP_3_EXP-tweet-Rt-report",
-        start_date   = datetime.datetime(2021, 4, 29),
-        http_conn_id = "cloud_functions",
-        data         = json.dumps({"state_code": state}),
-        retries      = 3
-    )
+def simulation_initial_conditions(state):
+    return DummyOperator(task_id = f"simulation_initial_conditions_{state}", 
+    start_date   = datetime.datetime(2021, 4, 29))
+    # return CloudFunction(
+    #     task_id      = f"simulation_initial_conditions_{state}",
+    #     method       = "POST",
+    #     endpoint     = "STEP_3_EXP-tweet-Rt-report",
+    #     start_date   = datetime.datetime(2021, 4, 29),
+    #     http_conn_id = "cloud_functions",
+    #     data         = json.dumps({"state_code": state}),
+    #     retries      = 3
+    # )
 
+def simulation_step(state):
+    return DummyOperator(task_id = f"simulation_step_{state}",
+    start_date   = datetime.datetime(2021, 4, 29))
+    # return CloudFunction(
+    #     task_id      = f"simulation_step_{state}",
+    #     method       = "POST",
+    #     endpoint     = "STEP_3_EXP-tweet-Rt-report",
+    #     start_date   = datetime.datetime(2021, 4, 29),
+    #     http_conn_id = "cloud_functions",
+    #     data         = json.dumps({"state_code": state}),
+    #     retries      = 3
+    # )
 
-with models.DAG("Rt_pipeline", schedule_interval = "45 8 * * *", catchup = False) as dag:
-    get_timeseries = CloudFunction(
-        task_id      = "get_timeseries",
-        method       = "POST",
-        endpoint     = "STEP_0_RAW-get-state-case-timeseries",
-        start_date   = datetime.datetime(2021, 4, 29),
-        http_conn_id = "cloud_functions"
-    )
+def get_dag(name: str, tweet: bool) -> models.DAG:
+    with models.DAG(name, schedule_interval = "45 8 * * *", catchup = False) as dag:
+        get_timeseries = CloudFunction(
+            task_id      = "get_timeseries",
+            method       = "POST",
+            endpoint     = "STEP_0_RAW-get-state-case-timeseries",
+            start_date   = datetime.datetime(2021, 4, 29),
+            http_conn_id = "cloud_functions"
+        )
 
-    get_vax_data = CloudFunction(
-        task_id      = "get_vax_data",
-        method       = "POST",
-        endpoint     = "STEP_0_RAW-get-vax-data",
-        start_date   = datetime.datetime(2021, 4, 29),
-        http_conn_id = "cloud_functions"
-    )
+        get_vax_data = CloudFunction(
+            task_id      = "get_vax_data",
+            method       = "POST",
+            endpoint     = "STEP_0_RAW-get-vax-data",
+            start_date   = datetime.datetime(2021, 4, 29),
+            http_conn_id = "cloud_functions"
+        )
 
-    simulation_initial_conditions = CloudFunction(
-        task_id      = "simulation_initial_conditions",
-        method       = "POST",
-        endpoint     = "STEP_1_EST-simulation-initial-conditions",
-        start_date   = datetime.datetime(2021, 4, 29),
-        http_conn_id = "cloud_functions"
-    )
+        fanout = DummyOperator(task_id = "fanout", start_date = datetime.datetime(2021, 4, 29))
 
-    for state in states:
-        epi_step_for_state = epi_step(state)
-        get_timeseries          >>\
-        epi_step_for_state      >>\
-        create_Rt_report(state) >>\
-        tweet_Rt_report(state)
+        [get_timeseries, get_vax_data] >> fanout
 
-        [get_vax_data, epi_step_for_state] >>\
-        simulation_initial_conditions      >>\
-        forward_simulation(state)
+        for state in states:
+            epi_step_for_state = epi_step(state)
+            fanout >> epi_step_for_state >> simulation_initial_conditions(state) >> simulation_step(state)
+            if tweet:
+                epi_step_for_state >> create_Rt_report(state) >> tweet_Rt_report(state)
+           
+        return dag 
+
+rt_pipeline          = get_dag("Rt_pipeline",          tweet = True)
+rt_pipeline_no_tweet = get_dag("Rt_pipeline_no_tweet", tweet = False)
---
+++ pipeline/est/simulation_initial_conditions/main.py
@@ -0,0 +1,160 @@
+from pathlib import Path
+
+import pandas as pd
+from epimargin.etl.covid19india import state_code_lookup, state_name_lookup
+from epimargin.smoothing import notched_smoothing
+from google.cloud import storage
+
+# model details 
+gamma     = 0.1 # 10 day infectious period
+window    = 10
+CI        = 0.95
+lookback  = 120 # how many days back to start estimation
+cutoff    = 2   # most recent data to use 
+excluded = ["Unknown", "Other State", "Airport Quarantine", "Railway Quarantine"]
+coalesce_states = ["Delhi", "Manipur", "Dadra And Nagar Haveli And Daman And Diu", "Andaman And Nicobar Islands"]
+survey_date = "October 23, 2020"
+columns  = ["state_code", "state", "district", "sero_0", "N_0", "sero_1", "N_1", "sero_2", "N_2", "sero_3", "N_3", "sero_4", "N_4", "sero_5", "N_5", "sero_6", "N_6", "N_tot", "Rt", "Rt_upper", "Rt_lower", "S0", "I0", "R0", "D0", "dT0", "dD0", "V0"]
+# cloud details 
+bucket_name = "daily_pipeline"
+
+def get(request, key):
+    request_json = request.get_json()
+    if request.args and key in request.args:
+        return request.args.get(key)
+    elif request_json and key in request_json:
+        return request_json[key]
+    else:
+        return None
+
+def assemble_data(request):
+    state_code = get(request, 'state_code')
+    state = state_code_lookup[state_code]
+
+    print(f"Assembling initial conditions for {state_code} ({state}).")
+    
+    bucket = storage.Client().bucket(bucket_name)
+    data = Path("/tmp")
+
+    bucket.blob("pipeline/commons/refs/all_india_sero_pop.csv")\
+        .download_to_filename(data / "all_india_sero_pop.csv")
+
+    bucket.blob("pipeline/raw/state_case_timeseries.csv")\
+        .download_to_filename(data / "state_case_timeseries.csv")
+
+    bucket.blob("pipeline/raw/district_case_timeseries.csv")\
+        .download_to_filename(data / "district_case_timeseries.csv")
+
+    bucket.blob("pipeline/raw/vaccine_doses_statewise.csv")\
+        .download_to_filename(data / "vaccine_doses_statewise.csv")
+
+    bucket.blob(f"pipeline/est/{state_code}_district_Rt.csv")\
+        .download_to_filename(data / f"{state_code}_district_Rt.csv")
+
+    bucket.blob(f"pipeline/est/{state_code}_state_Rt.csv")\
+        .download_to_filename(data / f"{state_code}_state_Rt.csv")
+    
+    print(f"Downloaded simulation input data for {state_code} ({state}).")
+
+    district_age_pop = pd.read_csv(data / "all_india_sero_pop.csv")      .set_index(["state", "district"])
+    
+    state_ts    = pd.read_csv(data / "state_case_timeseries.csv")   .set_index(["detected_state"])
+    district_ts = pd.read_csv(data / "district_case_timeseries.csv").set_index(["detected_state", "detected_district"]).loc[state]
+    
+    state_Rt    = pd.read_csv(data / f"{state_code}_state_Rt.csv",    index_col = 0, parse_dates = ["dates"])\
+        [["dates", "Rt_pred"]]\
+        .assign(district = state)\
+        .drop_duplicates(subset = "district", keep = "last")\
+        [["district", "Rt_pred"]]\
+        .set_index("district")
+    district_Rt = pd.read_csv(data / f"{state_code}_district_Rt.csv", index_col = 0, parse_dates = ["dates"])\
+        [["district", "dates", "Rt_pred"]]\
+        .drop_duplicates(subset = "district", keep = "last")\
+        [["district", "Rt_pred"]]\
+        .set_index("district")
+    
+    vax = pd.read_csv(data / "vaccine_doses_statewise.csv").set_index("State").T
+    vax.columns = vax.columns.str.title()
+    vax.set_index(pd.to_datetime(vax.index, format = "%d/%m/%Y"), inplace = True)
+
+    smooth = notched_smoothing(window = window)
+    simulation_start = pd.Timestamp.today() - pd.Timedelta(days = cutoff)
+
+    districts_to_run = district_age_pop.loc[state]
+    # if time series data not available at the district level, coalesce to state/UT level
+    if state in coalesce_states:
+        districts_to_run = districts_to_run\
+            .assign(**{f"infected_{i}": (lambda i: lambda _: _[f"sero_{i}"] * _[f"N_{i}"])(i) for i in range(7)})\
+            .drop(columns = [f"sero_{i}" for i in range(7)])\
+            .sum(axis = 0)\
+            .to_frame().T\
+            .assign(**{f"sero_{i}": (lambda i: lambda _: _[f"infected_{i}"] / _[f"N_{i}"])(i) for i in range(7)})\
+            [districts_to_run.columns]\
+            .assign(district = state)\
+            .set_index("district")
+        ts = state_ts
+        districts_to_run = districts_to_run.join(state_Rt)
+    else: 
+        ts = district_ts
+        districts_to_run = districts_to_run.join(district_Rt)
+    
+
+    print(f"Done reading input data for {state_code} ({state}).")
+    print(f"Running seroprevalence scaling for districts.")
+
+    rows = []
+    for _ in districts_to_run.dropna().itertuples():
+        print(f"Scaling for {state_code}/{district}.")
+        district, sero_0, sero_1, sero_2, sero_3, sero_4, sero_5, sero_6, N_0, N_1, N_2, N_3, N_4, N_5, N_6, N_tot, Rt = _
+
+        dR_conf = ts.loc[state, district].dR
+        dR_conf = dR_conf.reindex(pd.date_range(dR_conf.index.min(), dR_conf.index.max()), fill_value = 0)
+        if len(dR_conf) >= window + 1:
+            dR_conf_smooth = pd.Series(smooth(dR_conf), index = dR_conf.index).clip(0).astype(int)
+        else: 
+            dR_conf_smooth = dR_conf
+
+        R_conf_smooth  = dR_conf_smooth.cumsum().astype(int)
+        R_conf = R_conf_smooth[survey_date if survey_date in R_conf_smooth.index else -1]
+        R_sero = (sero_0*N_0 + sero_1*N_1 + sero_2*N_2 + sero_3*N_3 + sero_4*N_4 + sero_5*N_5 + sero_6*N_6)
+        R_ratio = R_sero/R_conf if R_conf != 0 else 1 
+        R0 = R_conf_smooth[simulation_start if simulation_start in R_conf_smooth.index else -1] * R_ratio
+        
+        dD_conf = ts.loc[state, district].dD
+        dD_conf = dD_conf.reindex(pd.date_range(dD_conf.index.min(), dD_conf.index.max()), fill_value = 0)
+        if len(dD_conf) >= window + 1:
+            dD_conf_smooth = pd.Series(smooth(dD_conf), index = dD_conf.index).clip(0).astype(int)
+        else:
+            dD_conf_smooth = dD_conf
+        D_conf_smooth  = dD_conf_smooth.cumsum().astype(int)
+        D0 = D_conf_smooth[simulation_start if simulation_start in D_conf_smooth.index else -1]
+        
+        dT_conf = ts.loc[state, district].dT
+        dT_conf = dT_conf.reindex(pd.date_range(dT_conf.index.min(), dT_conf.index.max()), fill_value = 0)
+        if len(dT_conf) >= window + 1:
+            dT_conf_smooth = pd.Series(smooth(dT_conf), index = dT_conf.index).clip(0).astype(int)
+        else:
+            dT_conf_smooth = dT_conf
+        T_conf_smooth  = dT_conf_smooth.cumsum().astype(int)
+        T_conf = T_conf_smooth[survey_date if survey_date in T_conf_smooth.index else -1]
+        T_sero = R_sero + D0 
+        T_ratio = T_sero/T_conf if T_conf != 0 else 1 
+        T0 = T_conf_smooth[simulation_start if simulation_start in T_conf_smooth.index else -1] * T_ratio
+
+        S0 = max(0, N_tot - T0)
+        dD0 = dD_conf_smooth[simulation_start if simulation_start in dD_conf_smooth.index else -1]
+        dT0 = dT_conf_smooth[simulation_start if simulation_start in dT_conf_smooth.index else -1] * T_ratio
+        I0 = max(0, (T0 - R0 - D0))
+
+        V0 = vax.loc[simulation_start][state] * N_tot / districts_to_run.loc[state].N_tot.sum()
+
+        rows.append((state_code, state, district, 
+            sero_0, N_0, sero_1, N_1, sero_2, N_2, sero_3, N_3, sero_4, N_4, sero_5, N_5, sero_6, N_6, N_tot, 
+            Rt, S0, I0, R0, D0, dT0, dD0, V0
+        ))
+    
+    pd.DataFrame(rows, columns = columns).to_csv(data / f"simulation_initial_conditions_{state_code}.csv")
+    bucket.blob(f"pipeline/sim/input/simulation_initial_conditions_{state_code}.csv")\
+        .upload_from_filename(str(data / f"simulation_initial_conditions_{state_code}.csv"), content_type = "text/csv")
+
+    return "OK!"
