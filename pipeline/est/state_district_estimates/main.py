3fb0dba940440c461596703aa971b0ea201ed5b9
satej soman <satej.soman@gmail.com>
2021-05-13 14:05:27 -0500

    normalize state name and wrap state-level estimation with try/catch

--- pipeline/est/state_district_estimates/main.py
+++ pipeline/est/state_district_estimates/main.py
@@ -59,34 +59,38 @@ def run_estimates(request):
         .rename(index = lambda s: s.replace(" and ", " & "), level = 0)\
         .loc[state]
     print(f"Estimating state-level Rt for {state_code}") 
-    (
-        dates,
-        Rt_pred, Rt_CI_upper, Rt_CI_lower,
-        T_pred, T_CI_upper, T_CI_lower,
-        total_cases, new_cases_ts, *_
-    ) = analytical_MPVS(
-        state_cases.iloc[-lookback:-cutoff].confirmed, 
-        CI = CI, smoothing = notched_smoothing(window = smoothing), totals = True
-    )
-    
-    lgd_state_name, lgd_state_id = crosswalk.query("state_api == @state").filter(like = "lgd_state").drop_duplicates().iloc[0]
+    normalized_state = state.replace(" and ", " & ")
+    lgd_state_name, lgd_state_id = crosswalk.query("state_api == @normalized_state").filter(like = "lgd_state").drop_duplicates().iloc[0]
+    try:
+        (
+            dates,
+            Rt_pred, Rt_CI_upper, Rt_CI_lower,
+            T_pred, T_CI_upper, T_CI_lower,
+            total_cases, new_cases_ts, *_
+        ) = analytical_MPVS(
+            state_cases.iloc[-lookback:-cutoff].confirmed, 
+            CI = CI, smoothing = notched_smoothing(window = smoothing), totals = True
+        )
 
-    pd.DataFrame(data = {
-        "dates": dates[1:],
-        "Rt_pred": Rt_pred,
-        "Rt_CI_upper": Rt_CI_upper,
-        "Rt_CI_lower": Rt_CI_lower,
-        "T_pred": T_pred,
-        "T_CI_upper": T_CI_upper,
-        "T_CI_lower": T_CI_lower,
-        "total_cases": total_cases[2:],
-        "new_cases_ts": new_cases_ts,
-    })\
-        .assign(state = state, lgd_state_name = lgd_state_name, lgd_state_id = lgd_state_id)\
-        .to_csv("/tmp/state_Rt.csv")
+        pd.DataFrame(data = {
+            "dates": dates[1:],
+            "Rt_pred": Rt_pred,
+            "Rt_CI_upper": Rt_CI_upper,
+            "Rt_CI_lower": Rt_CI_lower,
+            "T_pred": T_pred,
+            "T_CI_upper": T_CI_upper,
+            "T_CI_lower": T_CI_lower,
+            "total_cases": total_cases[2:],
+            "new_cases_ts": new_cases_ts,
+        })\
+            .assign(state = state, lgd_state_name = lgd_state_name, lgd_state_id = lgd_state_id)\
+            .to_csv("/tmp/state_Rt.csv")
 
-    # upload to cloud
-    bucket.blob(f"pipeline/est/{state_code}_state_Rt.csv").upload_from_filename("/tmp/state_Rt.csv",    content_type = "text/csv")
+        # upload to cloud
+        bucket.blob(f"pipeline/est/{state_code}_state_Rt.csv").upload_from_filename("/tmp/state_Rt.csv",    content_type = "text/csv")
+    except Exception as e:
+        print(f"ERROR when estimating Rt for {state_code}", e)
+        print(traceback.print_exc())
     
     if state in dissolved_states:
         print(f"Skipping district-level Rt for {state_code}")
@@ -94,7 +98,7 @@ def run_estimates(request):
         print(f"Estimating district-level Rt for {state_code}")
         estimates = []
         for district in filter(lambda _: _ not in excluded, district_cases.index.get_level_values(0).unique()):
-            lgd_district_data = crosswalk.query("state_api == @state & district_api == @district").filter(like = "lgd_district").drop_duplicates()
+            lgd_district_data = crosswalk.query("state_api == @normalized_state & district_api == @district").filter(like = "lgd_district").drop_duplicates()
             if not lgd_district_data.empty:
                 lgd_district_name, lgd_district_id = lgd_district_data.iloc[0]
             else:
