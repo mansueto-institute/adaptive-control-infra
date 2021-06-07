2baff1e21cbd24c20ee3b3e7fe1ed63e11cf4f82
Balaji Sathia Narayanan <balajivenkat96@gmail.com>
2021-06-06 20:26:28 -0700

    bug fix and using state code instead of state name in file names

--- pipeline/rpt/get_vaccine_statistics/main.py
+++ pipeline/rpt/get_vaccine_statistics/main.py
@@ -6,6 +6,48 @@ import os
 bucket_name = "daily_pipeline"
 bucket = storage.Client().bucket(bucket_name)
 
+state_lookup = {
+ 'Andaman & Nicobar Islands': 'AN',
+ 'Andhra Pradesh': 'AP',
+ 'Arunachal Pradesh': 'AR',
+ 'Assam': 'AS',
+ 'Bihar': 'BR',
+ 'Chandigarh': 'CH',
+ 'Chhattisgarh': 'CT',
+ 'Daman & Diu': 'DD',
+ 'Dadra & Nagar Haveli and Daman & Diu': 'DDDN',
+ 'Delhi': 'DL',
+ 'Dadra & Nagar Haveli': 'DN',
+ 'Goa': 'GA',
+ 'Gujarat': 'GJ',
+ 'Himachal Pradesh': 'HP',
+ 'Haryana': 'HR',
+ 'Jharkhand': 'JH',
+ 'Jammu & Kashmir': 'JK',
+ 'Karnataka': 'KA',
+ 'Kerala': 'KL',
+ 'Ladakh': 'LA',
+ 'Lakshadweep': 'LD',
+ 'Maharashtra': 'MH',
+ 'Meghalaya': 'ML',
+ 'Manipur': 'MN',
+ 'Madhya Pradesh': 'MP',
+ 'Mizoram': 'MZ',
+ 'Nagaland': 'NL',
+ 'Odisha': 'OR',
+ 'Punjab': 'PB',
+ 'Puducherry': 'PY',
+ 'Rajasthan': 'RJ',
+ 'Sikkim': 'SK',
+ 'Telangana': 'TG',
+ 'Tamil Nadu': 'TN',
+ 'Tripura': 'TR',
+ 'India': 'TT',
+ 'State Unassigned': 'UN',
+ 'Uttar Pradesh': 'UP',
+ 'Uttarakhand': 'UT',
+ 'West Bengal': 'WB'}
+
 def transfer_to_bucket(file_path):
     size_kb = os.stat(file_path).st_size / 1000
     print("Timeseries artifact size : {} KB".format(size_kb))
@@ -28,22 +70,22 @@ def generate_vax_report(_):
 
         # state level total aggregates
         totalStateDailyAgg = stateDF.groupby("date").sum()
-
+        state_code = state_lookup[state]
         totalStateDailyAgg["first_dose_admin"].plot()
-        plt.title("Number of first doses administered - {}".format)
-        plt.savefig("/tmp/first_dose_admin_{}.png".format(state))
+        plt.title("Number of first doses administered - {}".format(state))
+        plt.savefig("/tmp/first_dose_admin_{}.png".format(state_code))
         plt.close()
         print("Generated first dose statistics plot for {}".format(state))
 
         totalStateDailyAgg["total_individuals_registered"].plot()
         plt.title("Number of individuals registered - {}".format(state))
-        plt.savefig("/tmp/total_individuals_registered_{}.png".format(state))
+        plt.savefig("/tmp/total_individuals_registered_{}.png".format(state_code))
         plt.close()
         print("Generated total individuals registered plot for {}".format(state))
 
         totalStateDailyAgg["second_dose_admin"].plot()
         plt.title("Number of second doses administered - {}".format(state))
-        plt.savefig("/tmp/second_dose_admin_{}.png".format(state))
+        plt.savefig("/tmp/second_dose_admin_{}.png".format(state_code))
         plt.close()
         print("Generated second dose statistics plot for {}".format(state))
 
